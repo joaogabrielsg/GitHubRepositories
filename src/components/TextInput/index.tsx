ecd87bf03e845f6d00f7251d965e6c283e09879c
joao gabriel <joaogabrieldosantosgoncalves@gmail.com>
2021-03-31 00:24:44 -0300

    fix: text input component style

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import {TextInput, TextInputProps} from 'react-native';
+import {
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  Touchable,
+} from 'react-native';
+
+import Search from '../../../assets/search.png';
+
+import {theme} from '../../styles';
 
 interface CustomTextInputProps extends TextInputProps {
   text: string;
   onChangeText?: () => {};
+  onSearch?: () => {};
 }
 
-const CustomTextInput: React.FunctionComponent<CustomTextInputProps> = ({
-  text,
-  onChangeText,
-}) => {
+const CustomTextInput: React.FunctionComponent<CustomTextInputProps> = props => {
   return (
-    <TextInput
-      value={text}
-      onChangeText={onChangeText}
-      testID="nativeTextInput"
-    />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        value={props.text}
+        onChangeText={props.onChangeText}
+        testID="nativeTextInput"
+      />
+      <TouchableWithoutFeedback onPress={props.onSearch}>
+        <Image style={styles.image} source={Search} />
+      </TouchableWithoutFeedback>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: theme.secondary,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInput: {
+    flex: 1,
+  },
+  image: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain',
+  },
+});
 
 export default CustomTextInput;
