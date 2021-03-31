2fc40f098152b378984d9a6a4d336a103023fb92
joao gabriel <joaogabrieldosantosgoncalves@gmail.com>
2021-03-30 23:29:46 -0300

    style: add style file config

---
+++ src/styles/index.tsx
@@ -0,0 +1,11 @@
+export const theme = {
+  primary: 'rgb(224, 229, 240)',
+  secondary: 'rgb(255, 255, 255)',
+  textColor: 'rgb(0, 0, 0)',
+};
+
+export const font = {
+  title: 20,
+  subTitle: 17,
+  normal: 15,
+};
